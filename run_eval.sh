5b13cdf7674b289a8d669dab0356d4036890a793
Sorn Chottananurak <sorn111930@nmsl.kaist.ac.kr>
2023-12-15 02:05:46 +0900

    update

--- run_eval.sh
+++ run_eval.sh
@@ -1,15 +1,14 @@
-# CORRUPTIONS=("none" "gaussian_noise" "shot_noise" "impulse_noise" "speckle_noise" "gaussian_blur" "defocus_blur" "contrast" "brightness" "saturate")
-CORRUPTIONS=("impulse_noise")
-# CORRUPTIONS_LEVEL=(1 2 3 4 5)
-CORRUPTIONS_LEVEL=(5)
+CORRUPTIONS=("none" "gaussian_noise" "shot_noise" "impulse_noise" "speckle_noise" "gaussian_blur" "defocus_blur" "contrast" "brightness" "saturate")
+CORRUPTIONS_LEVEL=(1 2 3 4 5)
 
-GAME="ALE/MsPacman-v5" # fill this : ie ALE/Phoenix-v5
-CKPTPATH="runs/ALE/MsPacman-v5__MsPacman-v5_2M_BN_depth2__1__1700411267/MsPacman-v5_2M_BN_depth2.pth" # fill this : ie runs/ALE/Phoenix-v5__Phoenix-v5_2M_BN_depth1__1__1700541104/Phoenix-v5_2M_BN_depth1.pth
+
+GAME="" # fill this : ie ALE/Phoenix-v5
+CKPTPATH="" # fill this : ie runs/ALE/Phoenix-v5__Phoenix-v5_2M_BN_depth1__1__1700541104/Phoenix-v5_2M_BN_depth1.pth
 
 for C in "${CORRUPTIONS[@]}"; do
     for C_LVL in "${CORRUPTIONS_LEVEL[@]}"; do
-        python -W ignore dqn_eval.py --checkpoint_path ${CKPTPATH} --corruption_type ${C} --game_name ${GAME} --eval_eps 10 --corruption_level ${C_LVL} --device_id 0 --network_depth 2
-        python -W ignore dqn_eval.py --AdaDQN --checkpoint_path ${CKPTPATH} --corruption_type ${C} --game_name ${GAME} --eval_eps 10 --corruption_level ${C_LVL} --device_id 0 --network_depth 2
+        python -W ignore dqn_eval.py --checkpoint_path ${CKPTPATH} --corruption_type ${C} --game_name ${GAME} --eval_eps 1000 --corruption_level ${C_LVL} --device_id 0 --network_depth 2
+        python -W ignore dqn_eval.py --AdaDQN --checkpoint_path ${CKPTPATH} --corruption_type ${C} --game_name ${GAME} --eval_eps 1000 --corruption_level ${C_LVL} --device_id 0 --network_depth 2
 
     done
 done
--- run_train.sh
+++ run_train.sh
@@ -1,37 +1,12 @@
-python dqn_atari.py \
-    --exp-name MsPacman-v5_3M_BN_depth2 \
-    --capture-video \
-    --env-id ALE/MsPacman-v5 \
-    --total-timesteps 3000000 \
-    --buffer-size 400000 \
-    --save-model \
-    --network_depth 2\
+GAMES=("MsPacman" "AirRaid" "Phoenix" "SpaceInvaders")
 
-python dqn_atari.py \
-    --exp-name AirRaid-v5_3M_BN_depth2 \
-    --capture-video \
-    --env-id ALE/AirRaid-v5 \
-    --total-timesteps 3000000 \
-    --buffer-size 400000 \
-    --save-model \
-    --network_depth 2\
-
-python dqn_atari.py \
-    --exp-name Phoenix-v5_3M_BN_depth2 \
-    --capture-video \
-    --env-id ALE/Phoenix-v5 \
-    --total-timesteps 3000000 \
-    --buffer-size 400000 \
-    --save-model \
-    --network_depth 2\
-
-python dqn_atari.py \
-    --exp-name SpaceInvaders-v5_3M_BN_depth2 \
-    --capture-video \
-    --env-id ALE/SpaceInvaders-v5 \
-    --total-timesteps 3000000 \
-    --buffer-size 400000 \
-    --save-model \
-    --network_depth 2\
-
+for GAME in "${GAMES[@]}"; do
+    python -W ignore dqn_atari.py \
+            --exp-name ${GAMES}-v5_3M_BN_depth2 \
+            --env-id ALE/${GAMES}-v5 \
+            --total-timesteps 10000 \
+            --buffer-size 400000 \
+            --save-model \
+            --network_depth 2\
+done
 
