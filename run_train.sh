5af825458c389cac055f83e2b1f2f1127b2a15ed
leejunhee17 <jun17@kaist.ac.kr>
2023-12-15 14:37:26 +0900

    fix: typo in run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -2,8 +2,8 @@ GAMES=("MsPacman" "AirRaid" "Phoenix" "SpaceInvaders")
 
 for GAME in "${GAMES[@]}"; do
     python -W ignore dqn_atari.py \
-            --exp-name ${GAMES}-v5_3M_BN_depth2 \
-            --env-id ALE/${GAMES}-v5 \
+            --exp-name ${GAME}-v5_1M_BN_depth2 \
+            --env-id ALE/${GAME}-v5 \
             --total-timesteps 10000 \
             --buffer-size 400000 \
             --save-model \
